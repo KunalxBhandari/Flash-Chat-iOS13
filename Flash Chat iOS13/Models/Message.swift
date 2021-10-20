c1165bc80232bf28450fb2df0e352e5fb6b41ebe
Kunal Bhandari <kunalbhandari101@gmail.com>
2021-10-20 14:38:37 +0530

    Added all the functionality.

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Kunal Bhandari on 27/06/1400 AP.
+//  Copyright © 1400 AP Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
